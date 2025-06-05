6947a8b919f3ce057c0b0a1053d4b7e078744f12
chiancw <chiancw@gmail.com>
2025-06-05 15:06:57 +0800

    update output

--- outputs.fw.tf
+++ outputs.fw.tf
@@ -1,3 +1,7 @@
-output "firewall_rule_collection_group_id" {
-  value = azurerm_firewall_policy_rule_collection_group.example.id
+# output "firewall_rule_collection_group_id" {
+#   value = azurerm_firewall_policy_rule_collection_group.example.id
+# }
+
+output "avm_fwrcg_1_resource_id" {
+  value = module.avm_fwrcg_1.resource_id
 }
